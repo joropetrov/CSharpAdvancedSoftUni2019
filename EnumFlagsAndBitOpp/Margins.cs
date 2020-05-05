eab973ce14c9138de08bfd2886fc7d4a5f6b7cfa
Georgi Petrov <50695145+joropetrov@users.noreply.github.com>
2020-05-05 20:49:33 +0300

    Add files via upload

---
+++ EnumFlagsAndBitOpp/Margins.cs
@@ -0,0 +1,14 @@
+﻿namespace CombinableEnumValues
+{
+    using System;
+
+    [Flags]
+   public enum Margins
+    {
+        None = 0,
+        Top = 1 << 0,
+        Left = 1 << 1,
+        Bottom = 1<< 2,
+        Right = 1 << 3
+    }
+}
---
+++ EnumFlagsAndBitOpp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CombinableEnumValues
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bottomRightMargin = Margins.Bottom | Margins.Right;
+            var topLeftMargin = Margins.Top | Margins.Left;
+
+            //getting info
+            Console.WriteLine("borromRightMargin string value: {0}", bottomRightMargin);
+            Console.WriteLine("borromRightMargin integer value: {0}", (int)bottomRightMargin);
+            Console.WriteLine("borromRightMargin == Margins.Bottom: {0}", bottomRightMargin == Margins.Bottom);
+            Console.WriteLine(
+                "borromRightMargin has flag Margins.Bottom: {0}",
+                bottomRightMargin.HasFlag(Margins.Bottom));
+
+            //combining combinations
+            Console.WriteLine("bottomRightMargin and topLeftMargin: {0}", bottomRightMargin | topLeftMargin);
+
+            //Toggling vals
+            bottomRightMargin ^= Margins.Bottom;
+            Console.WriteLine("borromRightMargin ^= Margins.Bottom => {0}", bottomRightMargin);  
+            bottomRightMargin ^= Margins.Bottom;
+            Console.WriteLine("borromRightMargin ^= Margins.Bottom => {0}", bottomRightMargin);  
+
+        }
+    }
+}
