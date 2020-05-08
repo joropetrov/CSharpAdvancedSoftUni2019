135846452633568788a277e354f05e0d1010147a
Georgi Petrov <50695145+joropetrov@users.noreply.github.com>
2020-05-08 07:26:17 +0300

    Add files via upload

---
+++ IDisposableUnhandledMemo/DisposeImplementation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IDisposableUnhandledMemo
+{
+    public class DisposeImplementation : IDisposable
+    {
+        private bool isDisposed = false;
+        public DisposeImplementation()
+        {
+            Console.WriteLine("Creating obj of DisposeImplementation");
+        }
+        ~DisposeImplementation()
+        {
+            if (!isDisposed)
+            {
+                Console.WriteLine("Inside the finalizer of class DisposeImplementation");
+                this.Dispose();
+            }
+        }
+        public void Dispose()
+        {
+            isDisposed = true;
+            Console.WriteLine("Inside the dispose of class DisposeImplementation");
+
+            // WITH USING
+            //isDisposed = true;
+            //GC.SuppressFinalize(this);
+            //Console.WriteLine("inside the dispose of classs disposeimplementation");
+        }
+    }
+}
---
+++ IDisposableUnhandledMemo/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IDisposableUnhandledMemo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DisposeImplementation d = new DisposeImplementation();
+            d.Dispose();
+            d = null;
+            GC.Collect();
+            Console.ReadLine();
+
+            //WITH USING
+            //using (DisposeImplementation d = new DisposeImplementation())
+            //{
+            //    throw new Exception("In here");
+            //}
+        }
+    }
+}
