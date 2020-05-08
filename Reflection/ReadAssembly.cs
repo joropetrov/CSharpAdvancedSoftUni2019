2a721aace3a6ecd70c7408cce058dc92eac0cfd9
Georgi Petrov <50695145+joropetrov@users.noreply.github.com>
2020-05-08 07:46:17 +0300

    Add files via upload

---
+++ Reflection/ReadAssembly.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class ReadAssembly
+    {
+        static void Main(string[] args)
+        {
+            ReadAssemblyWithReflection();
+        }
+        static private void ReadAssemblyWithReflection()
+        {
+            string path = @"some path";
+            Assembly assembly = Assembly.LoadFile(path);
+
+            Type[] types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                Console.WriteLine("Class: " + type.Name);
+
+                MethodInfo[] methods = type.GetMethods();
+                foreach (var method in methods)
+                {
+                    Console.WriteLine("--Method: " + method.Name);
+
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (var param in parameters)
+                    {
+                        Console.WriteLine($"=== Parameter: {param.Name} : {param.ParameterType}");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
